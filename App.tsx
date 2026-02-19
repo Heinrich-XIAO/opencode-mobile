97ec1c9097f7a41fa8454ec7cb6a0b110cd189bc
Heinrich Xiao <74563446+Heinrich-XIAO@users.noreply.github.com>
2026-02-19 07:56:06 -0500

    Start on HostSelection instead of Home

--- App.tsx
+++ App.tsx
@@ -47,7 +47,7 @@ export default function App() {
             }}
           >
             <Stack.Navigator
-              initialRouteName="Home"
+              initialRouteName="HostSelection"
               screenOptions={{
                 headerShown: false,
               }}
